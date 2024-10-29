e92724cbe41d8beb34dd7c41ba055e57172ffae6
laguta-danil <laguta.d.s@gmail.com>
2024-10-29 14:45:43 +0200

    fix/react-env-rule

--- src/store/apiSettings.ts
+++ src/store/apiSettings.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEST_APP_API_URL, credentials: 'include' }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BACKEND_URL, credentials: 'include' }),
     endpoints: () => ({}),
     tagTypes: ['File', 'Folder'],
 });
